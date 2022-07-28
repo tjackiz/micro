b21e081b029321c9ff106d9f400571c31bbe147e
tjackiz <bhokenmaier@web.de>
2022-07-28 18:48:18 +0200

    [TASK] initial commit

---
+++ property-access-service/src/main/java/de/tjackiz/propertyaccessservice/PropertyAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package de.tjackiz.propertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyAccessServiceApplication.class, args);
+	}
+
+}
