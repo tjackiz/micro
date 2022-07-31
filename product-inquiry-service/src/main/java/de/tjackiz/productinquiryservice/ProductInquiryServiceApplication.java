bfd6f5b2bbee79f52c2de6af50770d701efdc3d3
tjackiz <bhokenmaier@web.de>
2022-07-31 16:02:43 +0200

    [TASK] enabled feign clients in main app class

--- product-inquiry-service/src/main/java/de/tjackiz/productinquiryservice/ProductInquiryServiceApplication.java
+++ product-inquiry-service/src/main/java/de/tjackiz/productinquiryservice/ProductInquiryServiceApplication.java
@@ -2,8 +2,10 @@ package de.tjackiz.productinquiryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("de.tjackiz.productinquiryservice")
 public class ProductInquiryServiceApplication {
 
 	public static void main(String[] args) {
