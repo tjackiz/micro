91415f9ca13a0967651f6c53e203a625ab729590
tjackiz <bhokenmaier@web.de>
2022-07-31 15:38:20 +0200

    [TASK] initial product inquiry service commit

---
+++ product-inquiry-service/src/main/java/de/tjackiz/productinquiryservice/ProductInquiryServiceApplication.java
@@ -0,0 +1,13 @@
+package de.tjackiz.productinquiryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInquiryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInquiryServiceApplication.class, args);
+	}
+
+}
---
+++ product-inquiry-service/src/test/java/de/tjackiz/productinquiryservice/ProductInquiryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tjackiz.productinquiryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInquiryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
